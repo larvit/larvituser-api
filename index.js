3efc3e659b483943c7dc4ae97639bad8f60f29e5
Klas Espenkrona <klas@larvit.se>
2018-01-03 14:02:00 +0100

    Updating to use new version of larvitbase-api

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 
 const	topLogPrefix	= 'larvituser-api: ./index.js - ',
 	userLib	= require('larvituser'),
-	Router	= require('larvitrouter'),
-	App	= require('larvitbase'),
+	App	= require('larvitbase-api'),
 	log	= require('winston');
 
 function Api(options) {
 	this.options	= options;
 
 	if ( ! this.options)	this.options	= {};
-	if ( ! this.options.appOptions)	this.options.appOptions	= {};
-	this.router	= new Router();
-}
+};
 
 Api.prototype.start = function (cb) {
 	const	logPrefix	= topLogPrefix + 'Api.prototype.start() - ',
@@ -37,51 +34,14 @@ Api.prototype.start = function (cb) {
 	userLib.ready(function (err) {
 		if (err) return cb(err);
 
-		if ( ! Array.isArray(that.options.appOptions.middleware)) {
-			that.options.appOptions.middleware	= [];
-		}
-
-		// Default to the latest version of the API
-		that.options.appOptions.middleware.push(function (req, res, cb) {
-			if ( ! req.url.match(/^\/v[0-9]\.[0-9]\//)) {
-				req.url	= '/v0.1' + req.url;
-			}
-			cb();
-		});
-
-		that.options.appOptions.middleware.push(function (req, res, cb) {
-			that.router.resolve(req.url, function (err, result) {
-				req.routed	= result;
-				cb(err);
-			});
-		});
-
-		that.options.appOptions.middleware.push(function (req, res, cb) {
-			if ( ! req.routed.controllerFullPath) {
-				res.statusCode	= 404;
-				res.data	= 'URL endpoint not found';
-			} else {
-				require(req.routed.controllerFullPath)(req, res, cb);
-			}
-		});
-
-		that.options.appOptions.middleware.push(function (req, res, cb) {
-			res.setHeader('Content-Type', 'application/json');
-			res.end(res.data);
-			cb();
-		});
-
-		that.app = new App(that.options.appOptions, function (err) {
-			if (err) return cb(err);
-			log.info(logPrefix + 'API is up and running!');
-			cb();
-		});
-
-		that.app.on('error', function (err, req, res) {
-			res.statusCode = 500;
-			res.end('Internal server error: ' + err.message);
-		});
+		that.app = new App(that.options);
+		that.app.start(cb);
 	});;
+};
+
+Api.prototype.stop = function (cb) {
+	const that = this;
+	that.app.close(cb);
 };
 
 exports = module.exports = Api;
