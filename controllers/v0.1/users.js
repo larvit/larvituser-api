00622bb78120f69de81012487843de730e92b6f3
Klas Espenkrona <klas@larvit.se>
2018-01-04 10:45:50 +0100

    Added tests for user listing

--- controllers/v0.1/users.js
+++ controllers/v0.1/users.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const logPrefix = 'larvituser-api ./controllers/api/v0.1/users.js - ',
+const topLogPrefix = 'larvituser-api ./controllers/api/v0.1/users.js - ',
 	userLib	= require('larvituser'),
-	async = require('async');
+	async = require('async'),
+	log	= require('winston');
 
-exports.run = function (req, res, cb) {
+exports = module.exports = function (req, res, cb) {
 	const	tasks = [];
 
-	let result;
-
 	if (req.method.toUpperCase() !== 'GET') {
+		log.info(topLogPrefix + 'Got request with unallowed method: "' + req.method + '", query: "' + req.urlParsed.href + '"');
 		res.setHeader('Content-Type', 'text/plain');
 		res.statusCode	= 405;
 		res.end('Method not allowed');
 		return cb(null, req, res, {});
 	}
 
+	log.verbose(topLogPrefix + 'Got request for users with query "' + req.urlParsed.href + '"');
+
 	tasks.push(function (cb) {
 		const users = new userLib.Users();
 		users.q	= req.urlParsed.query.q;
 		users.uuids	= req.urlParsed.query.uuids;
 		users.limit	= req.urlParsed.query.limit;
-		users.offset	= req.urlParsed.query.limit;
+		users.offset	= req.urlParsed.query.offset;
 		users.returnFields	= req.urlParsed.query.returnFields;
 
 		if (req.urlParsed.query.matchAllFields && req.urlParsed.query.matchAllFieldsValues) {
@@ -37,12 +39,12 @@ exports.run = function (req, res, cb) {
 		}
 
 		users.get(function (err, users) {
-			result = users;
+			res.data = users;
 			cb(err);
 		});
 	});
 
 	async.parallel(tasks, function (err) {
-		cb(err, req, res, result);
+		cb(err, req, res);
 	});
 };
